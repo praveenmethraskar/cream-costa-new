8ffa92b365acd6599e136bb2ffc5bacd37e7aec0
praveenmethraskar <praveen.methraskar@appprotech.in>
2026-02-22 11:47:55 +0530

    fix: mongo

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), "mongoose"];
-    }
-    return config;
+  turbopack: {
+    resolveExtensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
 };
 
-export default nextConfig;
+export default nextConfig
